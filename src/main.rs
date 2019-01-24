6f8f2279c7e440fd0d3378c8d63c6b27c6f23606
Neelay Junnarkar <neelay.junnarkar@gmail.com>
2019-01-24 04:33:55 +0000

    sql count and rate count

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use rusqlite::{Connection, NO_PARAMS};
 use std::process::Command;
 
 extern crate iron;
-use std::collections::{HashMap, VecDeque};
 use std::sync::{Arc,Mutex};
 use iron::prelude::*;
 use iron::status;
@@ -23,74 +22,71 @@ fn main() {
             )",
             NO_PARAMS
     ).expect("failed on create table");;
-    /*
-       conn.execute(
-       "CREATE TABLE IF NOT EXISTS counts (
-       ip TEXT NOT NULL,
-       count INTEGER NOT NULL
-       )",
-       NO_PARAMS
-       ).unwrap();
-       */
+
     let conn = Arc::new(Mutex::new(conn));
 
-    let counts: Arc<Mutex<HashMap<String, u64>>> = Arc::new(Mutex::new(HashMap::new()));
-
     Iron::new(move |req: &mut Request| {
-        if let Ok(mut counts) = counts.lock() {
-            if let Some(ip_addr_s) = req.headers.get::<XRealIP>() {
-                if let Ok(conn) = conn.lock() {
-                    let ip_addr_v = (&ip_addr_s).split(',').collect::<Vec<&str>>();
-                    let ip_addr = ip_addr_v[ip_addr_v.len()-1].trim().to_string();
+        if let Some(ip_addr_s) = req.headers.get::<XRealIP>() {
+            if let Ok(conn) = conn.lock() {
+                let ip_addr_v = (&ip_addr_s).split(',').collect::<Vec<&str>>();
+                let ip_addr = ip_addr_v[ip_addr_v.len()-1].trim().to_string();
 
-                    conn.execute(
-                        "INSERT INTO hits (ip)
+                conn.execute(
+                    "INSERT INTO hits (ip)
                             VALUES (?1)",
                             &[&ip_addr as &ToSql],
                             ).expect("failed on insert");
-                    /*
-                       conn.execute(
-                       "INSERT INTO counts (ip, count)
-                       VALUES (?1, ?2)
-                       ",
-                       &[&ip_addr as &ToSql],
-                       ).unwrap();
-                       */
 
-                    let mut response: String = String::new();
-                    response.push_str("Last 5\\n======\\n");
+                let mut response: String = String::new();
+                response.push_str("Last 5\\n======\\n");
 
-                    let mut stmt = conn.prepare("SELECT ip FROM hits GROUP BY ip HAVING timestamp=MAX(timestamp) ORDER BY timestamp DESC LIMIT 5").expect("failed on prepare");
-                    let mut rows = stmt.query(NO_PARAMS).expect("failed on query");
+                let mut stmt = conn.prepare("SELECT ip FROM hits GROUP BY ip HAVING timestamp=MAX(timestamp) ORDER BY timestamp DESC LIMIT 5").expect("failed on prepare");
+                let mut rows = stmt.query(NO_PARAMS).expect("failed on query");
 
-                    while let Some(row) = rows.next() {
-                        let row = row.unwrap();
-                        let ip: String = row.get(0);
-                        response.push_str(ip.as_str());
-                        response.push_str("\n");
-                    }
-
-                    {
-                        let count = (*counts).entry(ip_addr).or_insert(0);
-                        *count += 1;
-                    }
-
-                    let mut sorted = (*counts).iter().collect::<Vec<(&String, &u64)>>();
-                    sorted.sort_by(|a,b| b.1.cmp(a.1));
-                    let top5_str = sorted.iter().take(5).fold(String::new(), |acc, x| acc + x.0 + ": " + &x.1.to_string() + "\\n");
-
-
-                    response.push_str("\\nTop 5\\n=====\\n");
-                    response.push_str(&top5_str);
-
-                    response = String::from_utf8(Command::new("./cowsayer.sh")
-                                                 .args(vec![response])
-                                                 .output()
-                                                 .expect("failed to run cowsayer")
-                                                 .stdout).expect("failed to cvt output of cowsayer to string");
-
-                    return Ok(Response::with((status::Ok, response)));
+                while let Some(row) = rows.next() {
+                    let row = row.unwrap();
+                    let ip: String = row.get(0);
+                    response.push_str(ip.as_str());
+                    response.push('\n');
                 }
+
+                response.push_str("\\nTop 10\n======\\n");
+
+                let mut stmt = conn.prepare("SELECT ip, COUNT(*) AS count FROM hits GROUP BY ip ORDER BY count DESC LIMIT 10").expect("failed on other prepare");
+                let mut rows = stmt.query(NO_PARAMS).expect("failed on query2"); 
+
+                while let Some(row) = rows.next() {
+                    let row = row.unwrap();
+                    let ip: String = row.get(0);
+                    let count: i64 = row.get(1);
+                    response.push_str(ip.as_str());
+                    response.push_str(": ");
+                    response.push_str(count.to_string().as_str());
+                    response.push('\n');
+                }
+
+                let mut stmt = conn.prepare("SELECT ip, COUNT(*) AS count FROM hits WHERE timestamp>datetime('now', '-10 minute') GROUP BY ip ORDER BY count DESC LIMIT 10").expect("fail on rate prep");
+                let mut rows = stmt.query(NO_PARAMS).expect("failed on rate query");
+
+                response.push_str("\\nHits in last 10 min\\n===================\\n");
+
+                while let Some(row) = rows.next() {
+                    let row = row.unwrap();
+                    let ip: String = row.get(0);
+                    let count: i64 = row.get(1);
+                    response.push_str(ip.as_str());
+                    response.push_str(": ");
+                    response.push_str(count.to_string().as_str());
+                    response.push('\n');
+                }
+
+                response = String::from_utf8(Command::new("./cowsayer.sh")
+                                             .args(vec![response])
+                                             .output()
+                                             .expect("failed to run cowsayer")
+                                             .stdout).expect("failed to cvt output of cowsayer to string");
+
+                return Ok(Response::with((status::Ok, response)));
             }
         }
         Ok(Response::with((status::Ok, "something weird about how you're accessing this site\n"))) 
