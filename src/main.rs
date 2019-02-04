89db092895ac3b617b75cb4f9e3d07c1761c2658
Neelay Junnarkar <neelay.junnarkar@gmail.com>
2019-02-04 17:34:14 +0000

    removed some more excepts

--- src/main.rs
+++ src/main.rs
@@ -63,97 +63,100 @@ fn main() {
 
     Iron::new(move |req: &mut Request| {
         if let (Some(ip_addr_s), Ok(conn)) = (req.headers.get::<XRealIP>(), conns.lock()) {
-                let ip_addr_v = (&ip_addr_s).split(',').collect::<Vec<&str>>();
-                let ip_addr = ip_addr_v[ip_addr_v.len()-1].trim().to_string();
+            let ip_addr_v = (&ip_addr_s).split(',').collect::<Vec<&str>>();
+            let ip_addr = ip_addr_v[ip_addr_v.len()-1].trim().to_string();
 
-                conn.execute(
-                    "INSERT INTO hits (ip)
+            conn.execute(
+                "INSERT INTO hits (ip)
                             VALUES (?1)",
                             &[&ip_addr as &ToSql],
                             ).unwrap_or(0);
 
-                /* Check if ip already seen and otherwise geolookup and add to table */
-                if let Ok(mut stmt) = conn.prepare("SELECT 1 FROM geolookups WHERE ip=?1 LIMIT 1") {
-                    if let Ok(exists) = stmt.exists(&[&ip_addr as &ToSql]) {
-                        if !exists {
-                            /* geolookup ip */
-                            let mut easy = Easy::new();
-                            let mut data = Vec::new();
-                            if easy.url(&("http://ipinfo.io/".to_owned() + &ip_addr)).is_ok() {
-                                {
-                                    let mut transfer = easy.transfer();
-                                    transfer.write_function(|new_data| {
-                                        println!("{}",String::from_utf8(new_data.to_vec()).unwrap_or("failed to print".to_string()));
-                                        data.extend_from_slice(new_data);
-                                        Ok(new_data.len())
-                                    }).expect("fail on easy write fn");
-                                    transfer.perform().expect("fail on easy perform");
+            /* Check if ip already seen and otherwise geolookup and add to table */
+            if let Ok(mut stmt) = conn.prepare("SELECT 1 FROM geolookups WHERE ip=?1 LIMIT 1") {
+                if let Ok(exists) = stmt.exists(&[&ip_addr as &ToSql]) {
+                    if !exists {
+                        /* geolookup ip */
+                        let mut easy = Easy::new();
+                        let mut data = Vec::new();
+                        if easy.url(&("http://ipinfo.io/".to_owned() + &ip_addr)).is_ok() {
+                            let mut b = false;
+                            {
+                                let mut transfer = easy.transfer();
+                                if transfer.write_function(|new_data| {
+                                    println!("{}",String::from_utf8(new_data.to_vec()).unwrap_or("failed to print".to_string()));
+                                    data.extend_from_slice(new_data);
+                                    Ok(new_data.len())
+                                }).is_ok() {
+                                    b = transfer.perform().is_ok();
                                 }
+                            }
+                            if b {
                                 if let Ok(v) = serde_json::from_slice(&data) as serde_json::Result<Geolookup> {
                                     conn.execute(
                                         "INSERT INTO geolookups (ip, hostname, city, region, country, loc, postal, org)
-                                        VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8)",
-                                        &[&v.ip as &ToSql, &v.hostname as &ToSql, &v.city as &ToSql, &v.region as &ToSql, &v.country as &ToSql, &v.loc as &ToSql, &v.postal as &ToSql, &v.org as &ToSql],
-                                        ).expect("failed to insert into geolookups");
+                                                VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8)",
+                                                &[&v.ip as &ToSql, &v.hostname as &ToSql, &v.city as &ToSql, &v.region as &ToSql, &v.country as &ToSql, &v.loc as &ToSql, &v.postal as &ToSql, &v.org as &ToSql],
+                                                ).unwrap_or(0);
                                 }
                             }
                         }
                     }
                 }
-
-                /* Create response */
-                let mut response: String = String::new();
-
-                /* Add Last 5 */
-                response.push_str("Last 5\\n======\\n");
-                if let Ok(mut stmt) = conn.prepare("SELECT ip FROM hits GROUP BY ip HAVING timestamp=MAX(timestamp) ORDER BY timestamp DESC LIMIT 5") {
-                    if let Ok(mut rows) = stmt.query(NO_PARAMS) {
-                        while let Some(Ok(row)) = rows.next() {
-                            let ip: String = row.get(0);
-                            response.push_str(ip.as_str());
-                            response.push('\n');
-                        }
-                    }
-                }
-
-                /* Add Top 10*/
-                response.push_str("\\nTop 10\n======\\n");
-                if let Ok(mut stmt) = conn.prepare("SELECT ip, COUNT(*) AS count FROM hits GROUP BY ip ORDER BY count DESC LIMIT 10") {
-                    if let Ok(mut rows) = stmt.query(NO_PARAMS) {
-                        while let Some(Ok(row)) = rows.next() {
-                            let ip: String = row.get(0);
-                            let count: i64 = row.get(1);
-                            response.push_str(ip.as_str());
-                            response.push_str(": ");
-                            response.push_str(count.to_string().as_str());
-                            response.push('\n');
-                        }
-                    }
-                }
-
-                /* Add Top 10 in last 10 min */
-                response.push_str("\\nTop 10 in last 10 min\\n=====================\\n");
-                if let Ok(mut stmt) = conn.prepare("SELECT ip, COUNT(*) AS count FROM hits WHERE timestamp>datetime('now', '-10 minute') GROUP BY ip ORDER BY count DESC LIMIT 10") {
-                    if let Ok(mut rows) = stmt.query(NO_PARAMS) {
-                        while let Some(Ok(row)) = rows.next() {
-                            let ip: String = row.get(0);
-                            let count: i64 = row.get(1);
-                            response.push_str(ip.as_str());
-                            response.push_str(": ");
-                            response.push_str(count.to_string().as_str());
-                            response.push('\n');
-                        }
-                    }
-                }
-
-                response = String::from_utf8(Command::new("./cowsayer.sh")
-                                             .args(vec![response.clone()])
-                                             .output()
-                                             .expect("failed to run cowsayer")
-                                             .stdout).unwrap_or(response);
-
-                return Ok(Response::with((status::Ok, response)));
             }
+
+            /* Create response */
+            let mut response: String = String::new();
+
+            /* Add Last 5 */
+            response.push_str("Last 5\\n======\\n");
+            if let Ok(mut stmt) = conn.prepare("SELECT ip FROM hits GROUP BY ip HAVING timestamp=MAX(timestamp) ORDER BY timestamp DESC LIMIT 5") {
+                if let Ok(mut rows) = stmt.query(NO_PARAMS) {
+                    while let Some(Ok(row)) = rows.next() {
+                        let ip: String = row.get(0);
+                        response.push_str(ip.as_str());
+                        response.push('\n');
+                    }
+                }
+            }
+
+            /* Add Top 10*/
+            response.push_str("\\nTop 10\n======\\n");
+            if let Ok(mut stmt) = conn.prepare("SELECT ip, COUNT(*) AS count FROM hits GROUP BY ip ORDER BY count DESC LIMIT 10") {
+                if let Ok(mut rows) = stmt.query(NO_PARAMS) {
+                    while let Some(Ok(row)) = rows.next() {
+                        let ip: String = row.get(0);
+                        let count: i64 = row.get(1);
+                        response.push_str(ip.as_str());
+                        response.push_str(": ");
+                        response.push_str(count.to_string().as_str());
+                        response.push('\n');
+                    }
+                }
+            }
+
+            /* Add Top 10 in last 10 min */
+            response.push_str("\\nTop 10 in last 10 min\\n=====================\\n");
+            if let Ok(mut stmt) = conn.prepare("SELECT ip, COUNT(*) AS count FROM hits WHERE timestamp>datetime('now', '-10 minute') GROUP BY ip ORDER BY count DESC LIMIT 10") {
+                if let Ok(mut rows) = stmt.query(NO_PARAMS) {
+                    while let Some(Ok(row)) = rows.next() {
+                        let ip: String = row.get(0);
+                        let count: i64 = row.get(1);
+                        response.push_str(ip.as_str());
+                        response.push_str(": ");
+                        response.push_str(count.to_string().as_str());
+                        response.push('\n');
+                    }
+                }
+            }
+
+            response = match Command::new("./cowsayer.sh").args(vec![response.clone()]).output() {
+                Ok(x) => String::from_utf8(x.stdout).unwrap_or(response),
+                _ => response
+            };
+
+            return Ok(Response::with((status::Ok, response)));
+        }
         Ok(Response::with((status::Ok, "something weird about how you're accessing this site\n"))) 
     }).http("localhost:3001").unwrap();
 }
